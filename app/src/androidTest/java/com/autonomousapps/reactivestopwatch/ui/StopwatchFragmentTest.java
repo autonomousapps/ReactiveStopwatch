805424d915523b7e510c8af29e5b05e67748b280
Tony Robalik <tony.robalik@metova.com>
2016-09-17 18:05:59 -0500

    First Espresso test passing.

--- app/src/androidTest/java/com/autonomousapps/reactivestopwatch/ui/StopwatchFragmentTest.java
+++ app/src/androidTest/java/com/autonomousapps/reactivestopwatch/ui/StopwatchFragmentTest.java
@@ -1,5 +1,6 @@
 package com.autonomousapps.reactivestopwatch.ui;
 
+import com.autonomousapps.reactivestopwatch.R;
 import com.autonomousapps.reactivestopwatch.test.BaseEspressoTest;
 
 import org.junit.Before;
@@ -9,10 +10,18 @@ import org.junit.Test;
 import android.content.Intent;
 import android.support.test.rule.ActivityTestRule;
 
+import javax.inject.Inject;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
 public class StopwatchFragmentTest extends BaseEspressoTest {
 
-    @Rule
-    public ActivityTestRule<StopwatchActivity> activityRule = new ActivityTestRule<>(StopwatchActivity.class, true, false);
+    @Inject StopwatchMvp.Presenter stopwatchPresenter;
+
+    @Rule public ActivityTestRule<StopwatchActivity> activityRule = new ActivityTestRule<>(StopwatchActivity.class, true, false);
 
     @Before
     public void setup() throws Exception {
@@ -27,6 +36,12 @@ public class StopwatchFragmentTest extends BaseEspressoTest {
 
     @Test
     public void firstTest() throws Exception {
+        verifyUi();
+    }
 
+    private void verifyUi() throws Exception {
+        onView(withId(R.id.stopwatch)).check(matches(isDisplayed()));
+        onView(withId(R.id.btn_reset)).check(matches(isDisplayed()));
+        onView(withId(R.id.btn_start)).check(matches(isDisplayed()));
     }
 }
